684554ae30687105efc69dfb4440b6204ffa4456
Thomas Ashton <thomas.jude.ashton@gmail.com>
2026-09-01 12:10:05 +0100

    [RECAP] Variables and Strings
    Trivia bot exercise.

---
+++ learning/freecodecamp/variables-and-strings/javascript-trivia-bot/notes.md
@@ -0,0 +1,5 @@
+## Things Learnt through this excercise
+- Template Literal `text ${variable} more text`. It's more concise and easier to read.
+- how to change the value of a variable: 
+    - use "let *variable* =" to declare a variable
+    - then recall the variable without the "let" to change the content
---
+++ learning/freecodecamp/variables-and-strings/javascript-trivia-bot/script.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Tim";
+const botLocation = "UK";
+const favouriteLanguage = "English";
+
+console.log(`My name is ${botName} and I live on ${botLocation}`);
+console.log(`My favourite programming language is ${favouriteLanguage}`);
+
+let codingFact = "Javascript is really fun!";
+
+console.log(codingFact);
+
+codingFact = "Javascript is boring!!";
+
+console.log(codingFact);
